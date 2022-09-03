dd551c87d97e1f90dd7a0a68dce25fa3cc8cf014
Anillag <101154249+anillag@users.noreply.github.com>
2022-09-03 18:06:10 -0400

    Added components folder, integrated Header and Footer into App

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1 id="footer">Footer Content Goes Here</h1>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 id="header">Header Content Goes Here</h1>
+    </header>
+  );
+}
+
+export default Header;
