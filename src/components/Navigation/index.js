ff2d0c8e0c0d11a9b0ca63408d575b597ac347b1
Anillag <101154249+anillag@users.noreply.github.com>
2022-09-04 15:44:05 -0400

    Added Navigation and Contact components

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Navigation(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    contactSelected,
+    currentCategory,
+    setContactSelected,
+  } = props;
+
+  return (
+    <header className="">
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="">
+          <li className="">
+            <a href="#about" onClick={() => setContactSelected(false)}>
+              About Me
+            </a>
+          </li>
+          <li className={`${contactSelected && "navActive"}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`${
+                currentCategory.name === category.name &&
+                !contactSelected &&
+                "navActive"
+              }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
+              >
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
